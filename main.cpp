bfa9a8cc95bea491c936e522a61cc478218b221c
EldiShaba <101250259+EldiShaba@users.noreply.github.com>
2023-03-27 17:44:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
-int main() {
-    float a=0,b=0,c=0;
-    cin>>a>>b>>c;
-    cout<<(a+b)/2<<endl;
-    cout<<a*a<<endl;
-    cout<<a+b<<endl;
-    cout<<((a*b)*c)/2<<endl;
-    return 0;
+
+int main(){
+float a=0;
+float b=0;
+float c=0;
+cin >> a >> b >> c;
+    
+cout<< (a*b)/2 <<endl;
+cout<< (a*a) <<endl;
+cout<< (a*b) <<endl;
+cout<< (((a+b)*c)/2) <<endl;
+
 }
